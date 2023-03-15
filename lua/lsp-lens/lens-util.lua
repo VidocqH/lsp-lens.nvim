5657cb8da39cff8df415a4162c44b766dc25ce61
Jonathan Elize <jonathan.elize@gmail.com>
2023-03-15 17:49:11 -0500

    feat: ensure lense information show for items within an Interface and show lense information for Classes and Structs

--- lua/lsp-lens/lens-util.lua
+++ lua/lsp-lens/lens-util.lua
@@ -41,15 +41,26 @@ local SymbolKind = {
 local function get_functions(result)
   local ret = {}
   for _, v in pairs(result or {}) do
-    if v.kind == SymbolKind.Function or v.kind == SymbolKind.Methods or v.kind == SymbolKind.Interface then
+    local is_wrapper_structure = v.kind == SymbolKind.Class
+        or v.kind == SymbolKind.Struct
+        or v.kind == SymbolKind.Interface
+
+    if
+        v.kind == SymbolKind.Function
+        or v.kind == SymbolKind.Methods
+        or v.kind == SymbolKind.Interface
+        or is_wrapper_structure
+    then
       table.insert(ret, {
         name = v.name,
         rangeStart = v.range.start,
         selectionRangeStart = v.selectionRange.start,
         selectionRangeEnd = v.selectionRange["end"],
       })
-    elseif v.kind == SymbolKind.Class or v.kind == SymbolKind.Struct then
-      ret = utils:merge_table(ret, get_functions(v.children))   -- Recursively find methods
+    end
+
+    if is_wrapper_structure then
+      ret = utils:merge_table(ret, get_functions(v.children)) -- Recursively find methods
     end
   end
   return ret
